fb459d5e83f0ec2eeecfb4893501e82ef2474bb2
nicoduclare <nico@beb.xyz>
2023-02-13 21:07:37 -0800

    feat: added packages

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,5 @@
-import "./App.css";
-
 function App() {
-  return <div className="App">B7B</div>;
+  return <div className="App">The open-source BEB dimension browser</div>;
 }
 
 export default App;
