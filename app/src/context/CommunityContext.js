19d1b775183714a5f8261eb1977ec6358d5cceb3
nicoduclare <nico@beb.xyz>
2023-02-18 19:05:23 -0800

    feat: added community context

---
+++ app/src/context/CommunityContext.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { useQuery } from "@apollo/client";
+
+import { GET_COMMUNITY_BY_DOMAIN_OR_TOKEN_ID } from "../graphql/queries/GET_COMMUNITY_BY_DOMAIN_OR_TOKEN_ID";
+
+export const CommunityContext = React.createContext({
+  community: null,
+  loading: false,
+  error: null,
+});
+CommunityContext.displayName = "CommunityContext";
+
+export const useCommunityContext = () => React.useContext(CommunityContext);
+
+export const CommunityContextProvider = ({ children, domain, tld = "beb" }) => {
+  const { data, loading, error } = useQuery(
+    GET_COMMUNITY_BY_DOMAIN_OR_TOKEN_ID,
+    {
+      variables: {
+        bebdomain: domain,
+        tld,
+      },
+      skip: !domain,
+    }
+  );
+
+  const community = React.useMemo(() => {
+    return data?.CommunityQuery?.getCommunityByDomainOrTokenId;
+  }, [data]);
+
+  return (
+    <CommunityContext.Provider
+      value={{
+        community,
+        loading,
+        error,
+      }}
+    >
+      {children}
+    </CommunityContext.Provider>
+  );
+};
