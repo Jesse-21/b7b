855eb2c425c37f5712368665eff03ec913338220
nicoduclare <nico@beb.xyz>
2023-02-18 18:59:40 -0800

    added new graphql

---
+++ app/src/graphql/fragments/CORE_COMMUNITY_FIELDS.js
@@ -0,0 +1,29 @@
+import { gql } from "@apollo/client";
+import { CORE_ACCOUNT_FIELDS } from "./CORE_ACCOUNT_FIELDS";
+
+export const CORE_COMMUNITY_FIELDS = gql`
+  ${CORE_ACCOUNT_FIELDS}
+  fragment CoreCommunityFields on Community {
+    _id
+    name
+    description
+    bebdomain
+    bio {
+      raw
+      html
+    }
+    image {
+      src
+      isVerified
+      verificationExternalUrl
+    }
+    bannerImage {
+      src
+    }
+    owner {
+      ...CoreAccountFields
+    }
+    membersCount
+    tld
+  }
+`;
---
+++ app/src/graphql/queries/GET_COMMUNITY_BY_DOMAIN_OR_TOKEN_ID.js
@@ -0,0 +1,23 @@
+import { gql } from "@apollo/client";
+
+import { CORE_COMMUNITY_FIELDS } from "../fragments/CORE_COMMUNITY_FIELDS";
+
+export const GET_COMMUNITY_BY_DOMAIN = gql`
+  ${CORE_COMMUNITY_FIELDS}
+  query GET_COMMUNITY_BY_DOMAIN(
+    $bebdomain: String
+    $tokenId: String
+    $tld: String
+  ) {
+    CommunityQuery {
+      _id
+      getCommunityByDomainOrTokenId(
+        bebdomain: $bebdomain
+        tokenId: $tokenId
+        tld: $tld
+      ) {
+        ...CoreCommunityFields
+      }
+    }
+  }
+`;
