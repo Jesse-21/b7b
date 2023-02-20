e698ea9bfc5f9c039652408a9095a57606f717af
nicoduclare <nico@beb.xyz>
2023-02-19 16:18:37 -0800

    feat: working async apollo

--- src/containers/apollo/HostApolloProvider.js
+++ src/containers/apollo/HostApolloProvider.js
@@ -5,18 +5,32 @@ import { useParams } from "react-router-dom";
 import { makeApolloClient } from "../../helpers/make-apollo-client";
 
 export const HostApolloProvider = ({ children, dimension }) => {
-  const client = makeApolloClient(dimension);
+  const [client, setClient] = React.useState(null);
+
+  React.useEffect(() => {
+    let isMounted = true;
+    if (dimension) {
+      makeApolloClient(dimension).then((hostClient) => {
+        if (isMounted) setClient(hostClient);
+      });
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [dimension]);
+  if (!client) return <>Resolving host for {dimension}...</>;
+
   return <ApolloProvider client={client}>{children}</ApolloProvider>;
 };
 
-const MemoizedHostApolloProvider = React.memo(HostApolloProvider);
+const withParams = (Component) => {
+  const Memo = React.memo(Component);
 
-export const HostApolloProviderWithParams = ({ children }) => {
-  const { dimension } = useParams();
-
-  return (
-    <MemoizedHostApolloProvider dimension={dimension}>
-      {children}
-    </MemoizedHostApolloProvider>
-  );
+  // eslint-disable-next-line react/display-name
+  return ({ children }) => {
+    const { dimension } = useParams();
+    return <Memo dimension={dimension}>{children}</Memo>;
+  };
 };
+
+export const HostApolloProviderWithParams = withParams(HostApolloProvider);
