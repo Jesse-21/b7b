b25cc98ebd68fa03f0146b6bbf869c2422f8bbbd
nicoduclare <nico@beb.xyz>
2023-02-22 18:40:21 -0800

    feat: added header

--- src/containers/navigation/DimensionHeader.js
+++ src/containers/navigation/DimensionHeader.js
@@ -1,4 +1,4 @@
-import { Box } from "@chakra-ui/layout";
+import { Box, Text } from "@chakra-ui/layout";
 
 import { ProfileButton } from "../auth/ProfileButton";
 import { AuthModal } from "../auth/AuthModal";
@@ -7,8 +7,16 @@ import { AuthModalContextProvider } from "../../context/AuthModal";
 export const DimensionHeader = () => {
   return (
     <AuthModalContextProvider>
-      <Box display="flex" justifyContent={"space-between"}>
-        B7B
+      <Box
+        display="flex"
+        justifyContent={"space-between"}
+        alignItems="center"
+        py={2}
+        px={[2, null, null, 4]}
+      >
+        <Text fontWeight={"bold"} fontSize="2xl">
+          B7B
+        </Text>
         <ProfileButton />
       </Box>
       <AuthModal />
