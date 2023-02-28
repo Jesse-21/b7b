573d5d90b74337873706e1b1aef77254a6b843df
nicoduclare <nico@beb.xyz>
2023-02-27 18:33:00 -0800

    feat: added community image

--- src/containers/community/AdminCommunityImagesWithContext.js
+++ src/containers/community/AdminCommunityImagesWithContext.js
@@ -6,6 +6,7 @@ import { useCommunityContext } from "../../context/CommunityContext";
 import { ImageWithUpload } from "../../components/image/ImageWithUpload";
 
 import { useUploadImage } from "../../helpers/hooks/useUploadImage";
+import { useEditCommunity } from "../../helpers/hooks/useEditCommunity";
 import { useErrorToast } from "../../helpers/hooks/useErrorToast";
 
 const withCommunityContext = (Component) => {
@@ -14,19 +15,50 @@ const withCommunityContext = (Component) => {
   // eslint-disable-next-line react/display-name
   return () => {
     const { community } = useCommunityContext();
+    const { onEditCommunity } = useEditCommunity();
+    const onEdit = React.useCallback(
+      (values = {}) => {
+        if (!community?._id) return;
+        const variables = {
+          communityId: community._id,
+          bannerImageId: values.bannerImageId,
+          imageId: values.imageId,
+        };
+        onEditCommunity(variables);
+      },
+      [community?._id, onEditCommunity]
+    );
 
     return (
       <Memo
         bannerImageSrc={community?.bannerImage?.src}
         imageSrc={community?.image?.src}
+        onEdit={onEdit}
       />
     );
   };
 };
 
-const AdminCommunityImages = ({ bannerImageSrc, imageSrc }) => {
-  const { onImageUpload, loading, error } = useUploadImage();
-  useErrorToast(error);
+const AdminCommunityImages = ({ bannerImageSrc, imageSrc, onEdit }) => {
+  const { onImageUpload, loading, error, image } = useUploadImage();
+  const {
+    onImageUpload: onBannerImageUpload,
+    loading: bannerLoading,
+    error: bannerError,
+    image: bannerImage,
+  } = useUploadImage();
+
+  useErrorToast(error || bannerError);
+
+  React.useEffect(() => {
+    console.log("image", image?._id);
+    if (image?._id) {
+      onEdit({ imageId: image?._id });
+    }
+    if (bannerImage?._id) {
+      onEdit({ bannerImageId: bannerImage?._id });
+    }
+  }, [image?._id, bannerImage?._id]);
   return (
     <Box>
       <Box
@@ -37,18 +69,20 @@ const AdminCommunityImages = ({ bannerImageSrc, imageSrc }) => {
         borderBottomRightRadius={"2xl"}
       >
         <ImageWithUpload
-          onImageUpload={onImageUpload}
+          onImageUpload={onBannerImageUpload}
           w="100%"
           h="auto"
           objectFit={"cover"}
-          src={bannerImageSrc}
-          defaultSrc="/cover.jpg"
+          // src={bannerImage?.src || bannerImageSrc}
+          backgroundColor="gray.400"
+          borderColor="gray.400"
           rounded="md"
+          loading={bannerLoading}
         ></ImageWithUpload>
       </Box>
       <Box px={[2, null, null, 4]}>
         <ImageWithUpload
-          src={imageSrc}
+          src={image?.src || imageSrc}
           objectFit={"contain"}
           defaultSrc="/cover.jpg"
           mt={-16}
