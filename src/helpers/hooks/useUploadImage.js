523313de536f0f6907cd3d0104180f2871919a01
nicoduclare <nico@beb.xyz>
2023-02-28 20:08:03 -0800

    feat: added multiple upload image

--- src/helpers/hooks/useUploadImage.js
+++ src/helpers/hooks/useUploadImage.js
@@ -2,66 +2,78 @@
 import React from "react";
 
 export const useUploadImage = () => {
-  const [file, setFile] = React.useState(null);
-  const [image, setImage] = React.useState(null);
+  const [images, setImages] = React.useState([]);
   const [loading, toggleLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
 
-  const _beforeUploadCheck = (f) => {
+  const _beforeUploadCheck = React.useCallback((f) => {
     if (!f) {
-      setError("No file selected");
-      return false;
+      return "No file selected";
     }
     if (f.size && f.size > 1024 * 1024 * 10) {
-      setError("File must be less than 10 MB");
-      return false;
+      return "File must be less than 10 MB";
     }
-    return true;
-  };
+    return null;
+  }, []);
 
-  const onImageUpload = async (f) => {
-    if (!_beforeUploadCheck(f)) return;
-    setFile(f);
+  const _onImageUpload = React.useCallback(
+    async (f) => {
+      const _error = _beforeUploadCheck(f);
+      if (_error) throw new Error(_error);
+      try {
+        const formData = new FormData();
+        formData.append("files", f);
+        let uri = window.hostUri?.toString?.();
+        uri = uri.replace(/\/graphql$/, "");
+        const res = await fetch(`${uri}/image/upload`, {
+          method: "POST",
+          body: formData,
+        });
+        const resjson = await res.json();
+        if (resjson?.image) {
+          return resjson.image;
+        } else {
+          throw new Error(resjson?.message);
+        }
+      } catch (e) {
+        throw new Error(e.message);
+      }
+    },
+    [_beforeUploadCheck]
+  );
+
+  const onImageUpload = async (files = [], reset = true) => {
+    const targets = files?.length ? files : [files];
     toggleLoading(true);
     setError(null);
-    setImage(null);
     try {
-      const formData = new FormData();
-      formData.append("files", f);
-      let uri = window.hostUri?.toString?.();
-      uri = uri.replace(/\/graphql$/, "");
-      const res = await fetch(`${uri}/image/upload`, {
-        method: "POST",
-        body: formData,
-      });
-      const resjson = await res.json();
-      toggleLoading(false);
-      if (resjson.image) {
-        setImage(resjson.image);
+      const promises = await Promise.all(
+        targets.map(async (target) => {
+          return await _onImageUpload(target);
+        })
+      );
+      if (reset) {
+        setImages(promises);
       } else {
-        setError(resjson.message);
-        setFile(null);
+        setImages((_images) => [..._images, ...promises]);
       }
-      return resjson;
-    } catch (e) {
-      setFile(null);
       toggleLoading(false);
+    } catch (e) {
       setError(e.message);
     }
   };
 
   const reset = () => {
-    setFile(null);
-    setImage(null);
+    setImages([]);
     setError(null);
   };
 
   return {
-    file,
     onImageUpload,
     error: error,
     loading: loading,
-    image,
+    image: images?.[0],
+    images,
     reset,
   };
 };
