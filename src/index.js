e4995ce5150eb5d26e9418417d578050a199fadf
nicoduclare <nico@beb.xyz>
2023-03-01 21:44:37 -0800

    Revert "feat: tentatively use browser router with github"
    This reverts commit 3d99a5152e18cd08b33b379cdf858a1501bc7cc9.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./helpers/create-theme";
@@ -11,9 +11,9 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ChakraProvider theme={theme}>
-    <BrowserRouter>
+    <HashRouter>
       <AppRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </ChakraProvider>
 );
 
