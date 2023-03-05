1fc09cc6d84c844f11f8186d41cb89bf5f170a83
jcdenton.eth <jc@beb.xyz>
2023-03-04 18:30:03 -0800

    feat: static node.js

---
+++ static.js
@@ -0,0 +1,14 @@
+// serve static files from the docs directory
+const express = require("express");
+const app = express();
+app.use(express.static("docs"));
+
+// route everything to the index.html file
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root: "docs" });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
