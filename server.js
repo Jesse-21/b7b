ed3cbae92b2fa9efa8918d48fb01112e5805d419
jcdenton.eth <jc@beb.xyz>
2023-03-06 17:25:22 -0800

    fix: more seo

--- server.js
+++ server.js
@@ -6,10 +6,29 @@
 const express = require("express");
 const app = express();
 app.use(express.static("docs"));
+const fs = require("fs");
 
 // route everything to the index.html file
 app.get("*", (req, res) => {
-  res.sendFile("index.html", { root: "docs" });
+  // read the index.html file from the docs directory as a string
+  const index = fs.readFileSync("docs/index.html", "utf8");
+  let html = index;
+
+  console.log(req.originalUrl);
+
+  if (req.originalUrl) {
+    const title = "B7B, an open-source BEB dimension browser";
+
+    const newTitle = req.originalUrl.split("/")[1] + " - " + title;
+    // replace the string title with the req.query.url
+    html = index.replace(
+      `<title>${title}</title>`,
+      `<title>${newTitle}</title>`
+    );
+  }
+
+  // send the modified html file to the client
+  res.send(html);
 });
 
 const port = process.env.PORT || 3100;
